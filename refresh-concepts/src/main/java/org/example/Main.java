cdde880acb0f542236c09990e29a17f1a7b54505
David Barcenas <davidbarcenasmx@gmail.com>
2022-10-18 17:52:23 -0500

    fix db connection and hibernate dependency

--- refresh-concepts/src/main/java/org/example/Main.java
+++ refresh-concepts/src/main/java/org/example/Main.java
@@ -1,21 +1,11 @@
 package org.example;
 
+import org.example.hibernate.utils.HibernateUtil;
+import org.hibernate.Session;
+
 public class Main {
     public static void main(String[] args) {
-        Car clio = new Car();
-        clio.setBrand("Renault");
-        clio.setModel("Clio");
-        clio.setYear(2014);
-        clio.setKm(0);
-        clio.setDomain("MHN123");
-
-        Car megane = new Car("Renault", "Megane", 2016, 0, "ERT567");
-        Car megane2 = new Car("Renault", "Megane", 2016, 0, "ERT567");
-
-        System.out.println(clio.getBrand() + " - " + clio.getModel());
-        System.out.println(megane.getBrand() + " - " + megane.getModel());
-
-        System.out.println(clio.equals(megane));
-        System.out.println(megane.equals(megane2));
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.close();
     }
 }
---
+++ refresh-concepts/src/main/java/org/example/entities/AnimalEntity.java
@@ -0,0 +1,32 @@
+package org.example.entities;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class AnimalEntity implements Serializable {
+    @Id
+    private Integer id;
+    private String name;
+
+    public AnimalEntity() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
