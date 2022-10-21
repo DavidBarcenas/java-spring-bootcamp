608686b8c25c6a8c6585587c9ddfb1a5f3e05ce9
David Barcenas <davidbarcenasmx@gmail.com>
2022-10-21 12:59:26 -0500

    start project

---
+++ backend-university/src/main/java/com/spring/university/backenduniversity/BackendUniversityApplication.java
@@ -0,0 +1,13 @@
+package com.spring.university.backenduniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendUniversityApplication.class, args);
+	}
+
+}
---
+++ backend-university/src/test/java/com/spring/university/backenduniversity/BackendUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.university.backenduniversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
