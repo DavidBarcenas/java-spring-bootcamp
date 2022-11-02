84ece38d73bcce51de3f0c35dbbd3c970987e240
David Barcenas <davidbarcenasmx@gmail.com>
2022-11-01 21:18:44 -0600

    generate derived queries

--- backend-university/src/main/java/com/spring/university/backenduniversity/persistence/repository/CareerRepository.java
+++ backend-university/src/main/java/com/spring/university/backenduniversity/persistence/repository/CareerRepository.java
@@ -4,4 +4,7 @@ import com.spring.university.backenduniversity.persistence.entity.Career;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CareerRepository extends CrudRepository<Career, Integer> {
+    Iterable<Career> findByCareerContains(String careerName);
+    Iterable<Career> findByCareerContainsIgnoreCase(String careerName);
+    Iterable<Career> findCareerByTotalYearsAfter(Integer years);
 }
