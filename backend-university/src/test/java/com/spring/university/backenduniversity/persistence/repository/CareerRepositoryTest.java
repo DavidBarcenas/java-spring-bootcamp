f94681971d050612a4e7df1b5041ab7630ddf88b
David Barcenas <davidbarcenasmx@gmail.com>
2023-01-02 22:38:00 -0600

    setup h2 database for testing

---
+++ backend-university/src/test/java/com/spring/university/backenduniversity/persistence/repository/CareerRepositoryTest.java
@@ -0,0 +1,24 @@
+package com.spring.university.backenduniversity.persistence.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CareerRepositoryTest {
+
+    @Test
+    void findByNameContains() {
+    }
+
+    @Test
+    void findByNameContainsIgnoreCase() {
+    }
+
+    @Test
+    void findByTotalYearsAfter() {
+    }
+
+    @Test
+    void findCareersByTeacherNameAndLastname() {
+    }
+}
