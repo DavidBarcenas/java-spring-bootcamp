74afd32d8053f99e4e15953d5bfeeb13d4260bf7
David Barcenas <davidbarcenasmx@gmail.com>
2023-01-04 20:00:39 -0600

    test user, employee, student and teacher repository

--- backend-university/src/test/java/com/spring/university/backenduniversity/data/DummyData.java
+++ backend-university/src/test/java/com/spring/university/backenduniversity/data/DummyData.java
@@ -1,6 +1,8 @@
 package com.spring.university.backenduniversity.data;
 
-import com.spring.university.backenduniversity.persistence.entity.Career;
+import com.spring.university.backenduniversity.persistence.entity.*;
+
+import java.math.BigDecimal;
 
 public class DummyData {
     public static Career career1() {
@@ -11,5 +13,35 @@ public class DummyData {
     }
     public static Career career3() {
         return new Career(null, "Travel", 20, 2);
+    }
+
+    public static User employee1() {
+        return new Employee(null, "Juan", "Perez", "dfresdw4",
+                new Address(),
+                new BigDecimal(26000), EmployeeType.ADMINISTRATIVE);
+    }
+
+    public static User employee2() {
+        return new Employee(null, "Leandro", "Lopez", "lkjui23o",
+                new Address(),
+                new BigDecimal(20000), EmployeeType.MAINTENANCE);
+    }
+
+    public static User teacher1() {
+        return new Teacher(null, "Romina", "Sanchez", "asdasd34",
+                new Address(),
+                new BigDecimal(40000));
+    }
+
+    public static User studen1() {
+        return new Student(null, "Jhon", "Conde", "45gt65ht", new Address());
+    }
+
+    public static User studen2() {
+        return new Student(null, "Karla", "Ramirez", "loper4p5", new Address());
+    }
+
+    public static User studen3() {
+        return new Student(null, "Carlos", "Conde", "133lko6i", new Address());
     }
 }
---
+++ backend-university/src/test/java/com/spring/university/backenduniversity/persistence/repository/UserRepositoryTest.java
@@ -0,0 +1,62 @@
+package com.spring.university.backenduniversity.persistence.repository;
+
+import com.spring.university.backenduniversity.data.DummyData;
+import com.spring.university.backenduniversity.persistence.entity.Employee;
+import com.spring.university.backenduniversity.persistence.entity.Teacher;
+import com.spring.university.backenduniversity.persistence.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static com.spring.university.backenduniversity.data.DummyData.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class UserRepositoryTest {
+    @Autowired
+    @Qualifier("studentRepository")
+    UserRepository studentRepository;
+
+    @Autowired
+    @Qualifier("teacherRepository")
+    UserRepository teacherRepository;
+
+    @Autowired
+    @Qualifier("employeeRepository")
+    UserRepository employeeRepository;
+
+    @Test
+    void findByNameAndLastName() {
+        User newEmployee = employeeRepository.save(employee1());
+        Optional<User> employee = employeeRepository.findByNameAndLastName(employee1().getName(), employee1().getLastName());
+
+        assertThat(employee.get()).isInstanceOf(Employee.class);
+        assertThat(employee.get()).isEqualTo(newEmployee);
+    }
+
+    @Test
+    void findByIdDocument() {
+        User newTeacher = teacherRepository.save(teacher1());
+        Optional<User> teacher = teacherRepository.findByIdDocument(teacher1().getIdDocument());
+
+        assertThat(teacher.get()).isInstanceOf(Teacher.class);
+        assertThat(teacher.get()).isEqualTo(newTeacher);
+        assertThat(teacher.get().getIdDocument()).isEqualTo(newTeacher.getIdDocument());
+    }
+
+    @Test
+    void findByLastName() {
+        Iterable<User> newStudent = studentRepository.saveAll(Arrays.asList(studen1(), studen2(), studen3()));
+
+        String lastname = "Conde";
+        List<User> students = (List<User>) this.studentRepository.findByLastName(lastname);
+
+        assertThat(students.size() == 2).isTrue();
+    }
+}
