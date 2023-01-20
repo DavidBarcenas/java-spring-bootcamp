bb8c30ce655ebd84f230fbd37ed8be92e0db55b5
David Barcenas <davidbarcenasmx@gmail.com>
2023-01-20 15:35:46 -0600

    implement lombok

--- backend-university/src/main/java/com/spring/university/backenduniversity/persistence/dto/CareerDTO.java
+++ backend-university/src/main/java/com/spring/university/backenduniversity/persistence/dto/CareerDTO.java
@@ -1,42 +1,13 @@
 package com.spring.university.backenduniversity.persistence.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class CareerDTO {
     private Integer id;
     private String name;
     private Integer totalSubjects;
     private Integer totalYears;
-
-    public CareerDTO() {}
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getTotalSubjects() {
-        return totalSubjects;
-    }
-
-    public void setTotalSubjects(Integer totalSubjects) {
-        this.totalSubjects = totalSubjects;
-    }
-
-    public Integer getTotalYears() {
-        return totalYears;
-    }
-
-    public void setTotalYears(Integer totalYears) {
-        this.totalYears = totalYears;
-    }
 }
